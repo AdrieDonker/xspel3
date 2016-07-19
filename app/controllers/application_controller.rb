ef5fedc7870380baf394aa6f4585c42280b2999a
Adrie Donker <adrie.donker@gmail.com>
2016-07-19 12:57:30 +0000

    Versie 0.1 - Compleet, behalve spelen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,28 +1,28 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :set_locale
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
 
-  protected
+  private
 
+  # Sanitize devise permitted parameters
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:locale, :name, :email])
     # devise_parameter_sanitizer.permit(:sign_in, keys: [:locale]) n/n
     devise_parameter_sanitizer.permit(:account_update, keys: [:locale, :name, :email])
   end
   
-  private
-
-  # set the language
+  # Set the language
   def set_locale
     I18n.locale = current_user.try(:locale) || extract_locale_from_accept_language_header
   end
-
+  
+  # Extract language from HHTP-header
   def extract_locale_from_accept_language_header
-    logger.info '.. from header' 
-    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    lang = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+    logger.info "INFO: Language #{lang} determined from header"
+    lang
   end  
   # I18n.default_locale
 end
